00af3e1551507e049e89111b8495ccead4ab5639
flashdrag <flashdrag@gmail.com>
2023-09-11 13:00:54 +0100

    Configure Custom User; #8

--- cyclebay/settings.py
+++ cyclebay/settings.py
@@ -89,6 +89,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "cyclebay.wsgi.application"
 
+# Set custom user model as default
+AUTH_USER_MODEL = 'users.User'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
