9ae4138c65bd175b99835300a147ff8eeb85e4d3
flashdrag <flashdrag@gmail.com>
2023-09-12 18:09:54 +0100

    Create model Product

--- products/models.py
+++ products/models.py
@@ -13,3 +13,37 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Product(models.Model):
+    class Size(models.TextChoices):
+        SMALL = "S", "Small"
+        MEDIUM = "M", "Medium"
+        LARGE = "L", "Large"
+        XLARGE = "X", "XLarge"
+        OTHER = "O", "Other"
+
+    category = models.ForeignKey(
+        "Category",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="products",
+    )
+
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    size = models.CharField(
+        max_length=1,
+        choices=Size.choices,
+        default=Size.OTHER,
+    )
+    colour = models.CharField(max_length=50, null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
