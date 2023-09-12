f385e081da3b306aae672f7bf6fd2f028aaeac12
flashdrag <flashdrag@gmail.com>
2023-09-12 18:08:52 +0100

    Create model Category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
