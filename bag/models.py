1612f7b8544490be7017c950e7ccedf6fa4a0f2e
flashdrag <flashdrag@gmail.com>
2023-09-19 18:41:25 +0100

    Create model ProductReservation

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from products.models import ProductSize
+
+
+class ProductReservation(models.Model):
+    product_size = models.ForeignKey(
+        ProductSize, on_delete=models.CASCADE
+    )
+    quantity = models.PositiveIntegerField()
+    session_key = models.CharField(max_length=32)
+    reversed_at = models.DateTimeField(auto_now_add=True)
