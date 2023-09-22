2d33bf94b15004ef747cd6a1999a945129ec3b7a
flashdrag <flashdrag@gmail.com>
2023-09-22 14:19:31 +0100

    Create Order and OrderLineItem models

--- cyclebay/urls.py
+++ cyclebay/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("", include("home.urls")),
     path("products/", include("products.urls")),
     path('bag/', include('bag.urls')),
-    path('checkout/', include('checkout.urls')),
+    # TODO: Uncomment when checkout app is created
+    # path('checkout/', include('checkout.urls')),
     #  allows to serve media files in development
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
