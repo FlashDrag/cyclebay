8fbf48183eba7df4fcb412903c2450cb82ec4b71
flashdrag <flashdrag@gmail.com>
2023-09-22 14:25:18 +0100

    Add Order and OrderLineItem to admin panel

--- checkout/models.py
+++ checkout/models.py
@@ -11,6 +11,14 @@ from products.models import Product, ProductSize
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    # TODO: Add user profile to order model and admin
+    # user_profile = models.ForeignKey(
+    #     UserProfile,
+    #     on_delete=models.SET_NULL,
+    #     null=True,
+    #     blank=True,
+    #     related_name="orders",
+    # )
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
