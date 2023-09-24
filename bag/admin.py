1aa119b31c3440e09eea818447d576ebb5b121be
flashdrag <flashdrag@gmail.com>
2023-09-24 22:41:48 +0100

    Move ProductReservation model to checkout app

--- bag/admin.py
+++ bag/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-
-
-from .models import ProductReservation
-
-
-admin.site.register(ProductReservation)
--- bag/models.py
+++ bag/models.py
@@ -1,15 +1,1 @@
 from django.db import models
-
-from products.models import ProductSize
-
-
-class ProductReservation(models.Model):
-    product_size = models.ForeignKey(
-        ProductSize, on_delete=models.CASCADE
-    )
-    quantity = models.PositiveIntegerField()
-    session_key = models.CharField(max_length=32)
-    reversed_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"{self.product_size} - {self.quantity}"
