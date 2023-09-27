829f7107a1246d3e60a919da566336cba7d7475e
flashdrag <flashdrag@gmail.com>
2023-09-27 20:02:57 +0100

    Add wishlist views

--- cyclebay/urls.py
+++ cyclebay/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('wishlist/', include('wishlist.urls')),
     #  allows to serve media files in development
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
