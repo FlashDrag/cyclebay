b39903ed6beb21be8afe78ac10e4b0a2a01616a4
flashdrag <flashdrag@gmail.com>
2023-09-27 13:42:24 +0100

    Create Wishlist model

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    """
+    A Wishlist model for maintaining a list of favorite products
+    """
+    # ensures that each user can only have one wishlist
+    user_profile = models.OneToOneField(
+        UserProfile,
+        on_delete=models.CASCADE,
+        related_name='wishlist'
+    )
+    # User can add many products to their wishlist,
+    # and each product can be added to many wishlists
+    products = models.ManyToManyField(
+        Product,
+        related_name='wishlists',
+    )
+
+    def __str__(self):
+        return self.user_profile.user.email
