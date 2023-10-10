18f5c9782b80bdf362563649f73fbf0fe47983d1
flashdrag <flashdrag@gmail.com>
2023-10-10 17:49:58 +0100

    Setup AWS S3 for static and media files

--- cyclebay/settings.py
+++ cyclebay/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     # Other apps
     "crispy_forms",
     "crispy_bootstrap4",
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -185,13 +186,42 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_URL = '/static/'
-# Dir where static files will be collected using
-# python manage.py collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEVELOPMENT:
+    # aws settings
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = 'eu-west-1'  # Ireland
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
+    # s3 static settings
+    STATICFILES_LOCATION = 'static'
+    # URL path for your static files where they will be served from
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    STATICFILES_STORAGE = 'cyclebay.custom_storages.StaticStorage'
+
+    # s3 media settings
+    MEDIAFILES_LOCATION = 'media'
+    # URL path for media files where they will be served from
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'cyclebay.custom_storages.MediaStorage'
+else:
+    # URL path for your static files where they
+    # will be served from during development
+    STATIC_URL = '/static/'
+    # Dir where static files will be collected using
+    # python manage.py collectstatic
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # URL path for media files where they will be served from
+    MEDIA_URL = '/media/'
+    # Dir where media files are stored during development
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
